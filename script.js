3e0c6e1525dc07c1c70168bb80014f49740640cb
Jack Larkin <larkija33@gmail.com>
2021-01-29 00:02:49 -0500

    Created grid

---
+++ script.js
@@ -0,0 +1,22 @@
+const container = document.getElementById("container");
+
+function makeRows(rows, cols) {
+    container.style.setProperty('--grid-rows', rows);
+    container.style.setProperty('--grid-cols', cols);
+    for (c = 0; c < (rows * cols); c++) {
+        let cell = document.createElement("div");
+        //cell.innerText = (c + 1);
+        container.appendChild(cell).className = "grid-item";
+    };
+};
+
+makeRows(16, 16);
+
+function getRandomColor() {
+    var letters = '0123456789ABCDEF'.split('');
+    var color = '#';
+    for (var i = 0; i < 6; i++ ) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+   $('p').css('color',color);
+}
